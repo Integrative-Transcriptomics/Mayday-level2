ccd6ef7e476a937dc594d2c5db28266f0b09ea06
Adrian Geißler <adrian.geissler@student.uni-tuebingen.de>
2016-04-11 16:49:02 +0200

    Add AlignmentSummary

--- src/main/java/mayday/wapiti/experiments/generic/reads/bamsam/RNAseqStat.java
+++ src/main/java/mayday/wapiti/experiments/generic/reads/bamsam/RNAseqStat.java
@@ -11,25 +11,36 @@ import mayday.core.settings.generic.ObjectSelectionSetting;
 import mayday.core.settings.typed.BooleanSetting;
 import mayday.core.settings.typed.PathSetting;
 import mayday.core.tasks.AbstractTask;
-import mayday.vis3.plots.heatmap2.HeatMap;
 import mayday.vis3.tables.ExpressionTable;
+
+import org.apache.commons.compress.compressors.FileNameUtil;
+import picard.analysis.AlignmentSummaryMetrics;
+import picard.analysis.CollectAlignmentSummaryMetrics;
 import picard.analysis.CollectRnaSeqMetrics;
+import picard.analysis.RnaSeqMetrics;
+import picard.util.TabbedTextFileWithHeaderParser;
 
 import javax.swing.*;
-import javax.swing.text.Document;
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.zip.InflaterInputStream;
+import java.util.*;
 
 /**
  * Computes some statistics for SAM files and shows them to the user.
  * Created by adrian on 4/6/16.
+ *
+ * This classes uses the picard library. The individual tools are run with
+ * a String array list of options {"Option=Value", ...}.
  */
 public class RNAseqStat extends AbstractTask {
+
+    /**
+     * List of modes for strand specificity.
+     */
+    public static final String[] MODES
+            = new String[] {"NONE", "FIRST_READ_TRANSCRIPTION_STRAND",
+                    "SECOND_READ_TRANSCRIPTION_STRAND"};
+
     /**
      * paths of sam files.
      */
@@ -41,16 +52,28 @@ public class RNAseqStat extends AbstractTask {
     private Map<String, Map<String, Double>> statistics = new HashMap<>();
 
     /**
-     * Subset of Picard's values to show.
+     * Subset of Picard's values to show and their user-friendly explanation.
      * Full list:
+     * http://broadinstitute.github.io/picard/picard-metric-definitions.html#AlignmentSummaryMetrics
      * http://broadinstitute.github.io/picard/picard-metric-definitions.html#RnaSeqMetrics
      */
-    private final String[] SELECTION = new String[]{
-            "MEDIAN_CV_COVERAGE",
-            "PCT_RIBOSOMAL_BASES",
-            "PCT_CODING_BASES"
-    };
-
+    private HashMap<String, String> selection = new HashMap<String, String>() {{
+        // From Alignment Summary
+        put("TOTAL_READS", "# reads");
+        put("PF_READS_ALIGNED", "# aligned inc. ambiguities");
+        put("PF_HQ_ALIGNED_READS", "# hq reads, error chance < 1%");
+        put("MEAN_READ_LENGTH", "Mean read length");
+        put("PF_MISMATCH_RATE", "Mismatch rate for all reads");
+        put("PF_HQ_ERROR_RATE", "Mismatch rate hq");
+        put("PF_INDEL_RATE", "insertions/deletions per 100 aligned bases");
+        // From RNAseqMetrics
+        put("PCT_CODING_BASES", "% coding bases");
+        put("PCT_UTR_BASES", "% UTR bases");
+        put("PCT_INTRONIC_BASES", "% intronic bases");
+        put("PCT_INTERGENIC_BASES", "% intergenic bases");
+        put("MEDIAN_CV_COVERAGE", "Mean covariance top 1k transcripts (ideally 0)");
+        put("INCORRECT_STRAND_READS", "# reads aligned with incorrect strand.");
+    }};
 
     public RNAseqStat(List<String> samFiles) {
         super("Create Statistics for SAM files");
@@ -76,9 +99,7 @@ public class RNAseqStat extends AbstractTask {
         BooleanSetting sorted = new BooleanSetting("Sorted?",
                 "Are the files sorted", true);
         ObjectSelectionSetting strand = new ObjectSelectionSetting(
-                "Strand specificity", null, 1,
-                new String[] {"NONE", "FIRST_READ_TRANSCRIPTION_STRAND",
-                        "SECOND_READ_TRANSCRIPTION_STRAND"}
+                "Strand specificity", null, 1, MODES
         );
         PathSetting ref = new PathSetting("REF_FLAT", null, "", false, true, false);
 
@@ -106,17 +127,19 @@ public class RNAseqStat extends AbstractTask {
      */
     private ComponentPlaceHolderSetting explanation() {
         // TODO http://hgdownload.cse.ucsc.edu/downloads.html
-        //For strand-specific library prep. For unpaired reads, use FIRST_READ_TRANSCRIPTION_STRAND if the reads are expected to be on the transcription strand.
+        //For strand-specific library prep. For unpaired reads, use
+        // FIRST_READ_TRANSCRIPTION_STRAND if the reads are expected to be on
+        // the transcription strand.
         return new ComponentPlaceHolderSetting("", new JTextArea("Hello"));
     }
 
     /**
-     * Run Picard for the specified bam file 'in'.
+     * Run Picard's RNASeqMetric for the specified bam file 'in'.
      * @param in
      * @param out
      * @param settings
      */
-    private void runPicard(String in, String out, List<String> settings) {
+    private void runRNAMetric(String in, String out, List<String> settings) {
         // temporary settings object for this run
         List<String> runSettings =  new ArrayList<>(settings);
         runSettings.add("INPUT=" + in);
@@ -124,7 +147,34 @@ public class RNAseqStat extends AbstractTask {
 
         String[] s = runSettings.toArray(new String[0]);
         // run without exit
+        CollectRnaSeqMetrics prog = new CollectRnaSeqMetrics();
+        //RnaSeqMetrics
         (new CollectRnaSeqMetrics()).instanceMain(s);
+    }
+
+    /**
+     * Run Picard's RNASeqMetric for the specified bam file 'in'.
+     * @param in
+     * @param out
+     * @param settings
+     */
+    private void runSummary(String in, String out, List<String> settings) {
+        // temporary settings object for this run
+        List<String> runSettings =  new ArrayList<>();
+        runSettings.add("INPUT=" + in);
+        runSettings.add("OUTPUT=" + out);
+
+        // Keep RNAseqMetric specific settings out
+        // (Only AssumeSorted is of interest)
+        for (String set : settings) {
+            if (set.startsWith("ASSUME_SORTED=")) {
+                runSettings.add(set);
+            }
+        }
+
+        String[] s = runSettings.toArray(new String[0]);
+        // run without exit
+        (new CollectAlignmentSummaryMetrics()).instanceMain(s);
     }
 
     /**
@@ -133,38 +183,32 @@ public class RNAseqStat extends AbstractTask {
      * @param path
      */
     private void readPicard(Map<String, Double> storage, String path) {
-        final String DELIM = "\t";
-        try {
-            File result = new File(path);
-            List<String> lines = Files.readAllLines(result.toPath());
-
-            // The file has only two relevant lines
-            String[] header = null;
-            String[] values = null;
-            for (String l : lines) {
-                // ignore comments
-                if (l.length() == 0 || l.startsWith("#")) {
-                    continue;
-                }
-                if (header == null) {
-                    header = l.split(DELIM);
-                } else {
-                    // -1 limit to include every field, event trailing empty ones!
-                    values = l.split(DELIM, -1);
-                }
+        // Load file
+        TabbedTextFileWithHeaderParser parser
+                = new TabbedTextFileWithHeaderParser(new File(path));
+        // Load first row
+        Iterator<TabbedTextFileWithHeaderParser.Row> iter = parser.iterator();
+        TabbedTextFileWithHeaderParser.Row row = iter.next();
+        // Skip to correct row (tool specific)
+        // RNASeqMetrics -> First one (nothing to do)
+        // AlignmentSummary -> 'PAIR' or 'UNPAIRED' line
+        // Characteristic for AlignmentSummary is the existance of the CATEGORY
+        // colum.
+        if (parser.hasColumn("CATEGORY")) {
+            while (!(row.getField("CATEGORY").equals("UNPAIRED") ||
+                    row.getField("CATEGORY").equals("PAIR"))) {
+                row = iter.next();
             }
-            // Fill storage
-            assert header.length == values.length;
-            for (int i=0; i < header.length; i++) {
-                String f = values[i];
-                // use nan for empty fields
-                Double value = f.length() == 0
-                        ? Double.NaN
-                        : Double.valueOf(f);
-                storage.put(header[i], value);
+        }
+        for (String metric : selection.keySet()) {
+            try {
+                storage.put(metric, Double.valueOf(row.getField(metric)));
+            } catch (NumberFormatException e) {
+                System.err.println("Non numeric value for metric " + metric + "found, will use NAN");
+                storage.put(metric, Double.NaN);
+            } catch (NoSuchElementException e) {
+                // Field could be in other file -> ignore
             }
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Could not read Picard results: " + path);
         }
     }
 
@@ -176,20 +220,21 @@ public class RNAseqStat extends AbstractTask {
             // user cancelled
             return;
         }
+        // only one tmp file, will be overwritten by each picard run
+        String tmp = newTMP();
         for (String sam : samFiles) {
             // Prepare Map for Storage
             Map<String, Double> data = new HashMap<>();
             statistics.put(sam, data);
-            // run Picard
-            String tmp = newTMP();
-            if(tmp == null) {
-                return;
-            }
-            runPicard(sam, tmp, settings);
+            // run RNASeqMetrics
+            runRNAMetric(sam, tmp, settings);
             readPicard(data, tmp);
-            // cleanup
-            delTMP(tmp);
+            // run Summary tool (overwrite last tmp)
+            runSummary(sam, tmp, settings);
+            readPicard(data, tmp);
         }
+        // cleanup
+        delTMP(tmp);
         showData();
     }
 
@@ -197,13 +242,13 @@ public class RNAseqStat extends AbstractTask {
      * Create a path to a temporary file.
      * @return
      */
-    private String newTMP() {
+    private String newTMP() throws IOException{
         try {
             return File.createTempFile("mayday-", ".tsv").getAbsolutePath();
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not create temporary file for Picard.");
+            throw e;
         }
-        return null;
     }
 
     /**
@@ -224,18 +269,17 @@ public class RNAseqStat extends AbstractTask {
         ProbeList data = new ProbeList(ds, true);
         // create Columns
         for (String sam : samFiles) {
-            // TODO only basename
-            tmt.addExperiment(new Experiment(tmt, sam));
+            String basename = new File(sam).getName();
+            tmt.addExperiment(new Experiment(tmt, basename));
         }
         // Fill rows
-        for (String row : SELECTION) {
+        for (String metric : selection.keySet()) {
             Probe pb = new Probe(tmt, true);
-            // TODO map it
-            pb.setName(row);
+            pb.setName(selection.get(metric));
 
             double[] values = new double[samFiles.size()];
             for(int i=0; i < samFiles.size(); i++) {
-                values[i] = statistics.get(samFiles.get(i)).get(row);
+                values[i] = statistics.get(samFiles.get(i)).get(metric);
             }
 
             pb.setValues(values);
